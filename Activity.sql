52b65f1b282f13743188ad4d1aa5aff5c758aa70
lauravat <112356807+lauravat@users.noreply.github.com>
2023-05-16 17:51:08 -0500

    Update Activity.sql

--- Activity.sql
+++ Activity.sql
@@ -12,3 +12,33 @@ user_password varbinary(255) not null
 insert into users_tbl(user_firstname, user_lastname, user_email, user_password) values (upper('Laura Valentina'), upper('Aguilar Talero'), 'lau3232435124@gmail.com', aes_encrypt('Laura_Vat-302_4','$2a$12$CQCmQjJfHaAMKeFhg/Kipus9cUm6cjcX2AfOmlQleeu0cuMXOqn0G'));
 select *, cast(aes_decrypt(user_password,'$2a$12$CQCmQjJfHaAMKeFhg/Kipus9cUm6cjcX2AfOmlQleeu0cuMXOqn0G') as char (50))end_data from users_tbl where user_id=1; 
 select * from users_tbl;
+
+
+
+///////////////
+create database Activity;
+use Activity;
+
+create table users_tbl(
+user_id int primary key auto_increment not null,
+user_firstname varchar(50) not null,
+user_lastname varchar(50) not null,
+user_email varchar(80) not null,
+user_password varbinary(255) not null
+);
+
+create table categories(
+category_id int primary key auto_increment not null,
+category_name varchar(50) not null
+);
+
+create table products(
+product_id int primary key auto_increment not null,
+product_name varchar(50) not null,
+product_value varchar(50) not null
+);
+
+
+insert into users_tbl(user_firstname, user_lastname, user_email, user_password) values (upper('Laura Valentina'), upper('Aguilar Talero'), 'lau3232435124@gmail.com', aes_encrypt('Laura_Vat-302_4','$2a$12$CQCmQjJfHaAMKeFhg/Kipus9cUm6cjcX2AfOmlQleeu0cuMXOqn0G'));
+select *, cast(aes_decrypt(user_password,'$2a$12$CQCmQjJfHaAMKeFhg/Kipus9cUm6cjcX2AfOmlQleeu0cuMXOqn0G') as char (50))end_data from users_tbl where user_id=1; 
+select * from users_tbl;
